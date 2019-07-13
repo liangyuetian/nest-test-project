cce7e4a66de9b472834d9c0ef7e036ac06c7795d
王闯 <pt_liangyue@outlook.com>
2019-07-13 23:22:01 +0800

    添加自定义装饰器

---
+++ src/cats/cats.decorator.ts
@@ -0,0 +1,6 @@
+import { createParamDecorator, Request} from '@nestjs/common';
+
+export const Cats = createParamDecorator((data, req: Request) => {
+  // data 为装饰器传的参数，req 是响应体
+  return req.headers;
+});
