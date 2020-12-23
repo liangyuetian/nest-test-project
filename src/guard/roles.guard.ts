364140985a8fe818ca8c130ef389147209adb228
王闯 <wangchuang@ymt360.com>
2020-12-23 19:31:17 +0800

    添加守卫

--- src/guard/roles.guard.ts
+++ src/guard/roles.guard.ts
@@ -1,23 +1,32 @@
-import { Injectable, CanActivate, ExecutionContext, Request } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  Request,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
 
-function validateRequest(request: Request, context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    console.log('roles');
-    return true;
+function validateRequest(
+  request: Request,
+  context: ExecutionContext,
+): boolean | Promise<boolean> | Observable<boolean> {
+  console.log('roles');
+  return true;
 }
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(private readonly reflector: Reflector) {
-    }
+  constructor(private readonly reflector: Reflector) {}
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const roles = this.reflector.get<string[]>('roles', context.getHandler());
-        if (!roles) {
-            return true;
-        }
-        const request = context.switchToHttp().getRequest();
-        return validateRequest(request, context);
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
     }
+    const request = context.switchToHttp().getRequest();
+    return validateRequest(request, context);
+  }
 }
