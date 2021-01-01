efe74214377c4594723f6535a29dec04d7e1240c
pt <pt_liangyue@outlook.com>
2021-01-02 00:06:09 +0800

    update cats

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsMockController } from './cats.mock.controller';
-// import { RolesGuard } from '../guard/roles.guard';
+import { RolesGuard } from '../guard/roles.guard';
 import { LoggingInterceptor } from '../interceptor/logging.interceptor';
 import { CatsService } from './cats.service';
 
 @Module({
     controllers: [CatsController],
     providers: [
-        // {
-        //     provide: 'APP_GUARD', // 生效在全局
-        //     useClass: RolesGuard,
-        // },
+        {
+            provide: 'APP_GUARD', // 生效在全局
+            useClass: RolesGuard,
+        },
         {
             provide: 'APP_INTERCEPTOR', // 生效在全局
             useClass: LoggingInterceptor,
         },
-        {
-            provide: CatsController,
-            useValue: CatsMockController,
-        },
+        // {
+        //     provide: CatsController,
+        //     useValue: CatsMockController,
+        // },
         CatsService,
     ],
 })
