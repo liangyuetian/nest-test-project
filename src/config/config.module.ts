230aae0db149e0fc806baac8f9a914e5338e958c
pt <pt_liangyue@outlook.com>
2021-01-02 11:01:58 +0800

    update config.module.ts

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,12 +1,16 @@
-import { join } from 'path';
-import { Module } from '@nestjs/common';
-import { ConfigService } from './config.service';
+import {Module} from '@nestjs/common';
+// import {ConfigService} from './config.service';
+import {ConfigService} from '@nestjs/config';
 
 @Module({
-    providers: [{
-        provide: ConfigService,
-        useValue: new ConfigService(`${join(__dirname, '../../src/config', process.env.NODE_ENV)}.env`),
-    }],
+    imports: [],
+    providers: [
+        ConfigService,
+        // {
+        //     provide: ConfigService,
+        //     useValue: new ConfigService(`${join(__dirname, '../../src/config', process.env.NODE_ENV)}.env`),
+        // }
+    ],
     exports: [ConfigService],
 })
 export class ConfigModule {
